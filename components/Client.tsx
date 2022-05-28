e287ac2dd6fa6f99af54246291f7accc401b05b5
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-28 20:53:34 +0530

    updated

--- components/Client.tsx
+++ components/Client.tsx
@@ -2,14 +2,21 @@
 import React from "react";
 
 export default function Client() {
-  const image = ["/Airbnb.svg", "/FedEX.svg", "/Google.svg", "/Microsoft.svg"];
+  const image = ["/Airbnb.svg", "/FedEx.svg", "/Google.svg", "/Microsoft.svg"];
   return (
     <div className="container mx-auto px-5 text-xl flex flex-col items-start">
       <h1>Some of the clients I have designed for</h1>
       <div className="flex justify-between px-5 container mx-auto flex-wrap py-5">
         {image.map((n, index) => {
           return (
-            <img key={index} src={n} alt="image" width="140" height="140" className="cursor-pointer" />
+            <img
+              key={index}
+              src={n}
+              alt="image"
+              width="140"
+              height="140"
+              className="cursor-pointer"
+            />
           );
         })}
       </div>
