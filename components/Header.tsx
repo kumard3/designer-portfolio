823da2a36740a340e44371a0689fc0bf7f726ed3
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-28 15:38:39 +0530

    arrow added ↓

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
       <NavComponent />
-      <div id="home" className="h-screen w-full">
+      <div id="home" className="h-screen w-full flex justify-center flex-col items-center">
         <div className=" px-4 h-full flex justify-center flex-col items-center text-center">
           <h1 className="font-WorkSansRegular flex flex-col text-gray-500  text-4xl lg:text-6xl">
             <span>
@@ -22,6 +22,22 @@ export default function Header() {
             </>
           </h1>
         </div>
+        <svg
+          width="24"
+          height="14"
+          viewBox="0 0 24 14"
+          fill="none"
+           className="mb-16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 2L12 12L22 2"
+            stroke="#E3E4E6"
+            strokeWidth="2.67"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
     </>
   );
