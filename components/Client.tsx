918eeeee41c6e6eb7f187fc9416a8590ad61e723
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-28 21:16:43 +0530

    new section added

--- components/Client.tsx
+++ components/Client.tsx
@@ -5,7 +5,11 @@ export default function Client() {
   const image = ["/Airbnb.svg", "/FedEx.svg", "/Google.svg", "/Microsoft.svg"];
   return (
     <div className="container mx-auto px-5 text-xl flex flex-col items-start">
-      <h1>Some of the clients I have designed for</h1>
+      <h1 className="uppercase text-start text-3xl py-5 font-WorkSansSemiBold text-gray-500">
+        Some{" "}
+        <span className="text-white">of the clients I have designed for</span>
+      </h1>
+
       <div className="flex justify-between px-5 container mx-auto flex-wrap py-5">
         {image.map((n, index) => {
           return (
