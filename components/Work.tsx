77d7ad989939bd8d6387d2e6f680e49cac2eea5e
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-28 20:34:26 +0530

    almost done

--- components/Work.tsx
+++ components/Work.tsx
@@ -3,16 +3,15 @@ import Card from "./Card";
 
 export default function Work() {
   return (
-    <div
-      id="work"
-      className=" flex flex-col  items-center min-h-screen w-full "
-    >
-      <h1>work</h1>
-      <Card rev="reverse" />
-      <Card rev="" />
-      <Card rev="reverse" />
-      <Card rev="" />
-      <Card rev="reverse" />
+    <div id="work" className="min-h-screen px-4">
+      <h1 className="uppercase text-start text-xl">Selected Works</h1>
+      <div className=" flex flex-col  items-center  w-full ">
+        <Card rev="reverse" />
+        <Card rev="" />
+        <Card rev="reverse" />
+        <Card rev="" />
+        <Card rev="reverse" />
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import About from "../components/About";
+import Client from "../components/Client";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Work from "../components/Work";
 
@@ -7,8 +9,10 @@ export default function Home() {
   return (
     <div className="bg-black text-white w-full h-full font-WorkSansRegular">
       <Header />
+      <Client />
       <About />
       <Work />
+      <Footer />
     </div>
   );
 }
