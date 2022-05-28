62e0ee7f5aff0424834f168117880fbfa2fde757
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-28 15:30:13 +0530

    updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,19 @@ export default function Header() {
       <NavComponent />
       <div id="home" className="h-screen w-full">
         <div className=" px-4 h-full flex justify-center flex-col items-center text-center">
-          <h1 className="font-WorkSansRegular  text-gray-500  text-4xl lg:text-6xl">
-            I'm a designer specialising in
-            <span className="px-2 font-WorkSansSemiBold text-white">UI/UX</span>
-            and
-            <span className="px-2 font-WorkSansSemiBold text-white">
-              Interaction Design
+          <h1 className="font-WorkSansRegular flex flex-col text-gray-500  text-4xl lg:text-6xl">
+            <span>
+              I'm a designer specialising in
+              <span className="px-2 font-WorkSansSemiBold text-white">
+                UI/UX
+              </span>
+              and
             </span>
+            <>
+              <span className="px-2 font-WorkSansSemiBold text-white">
+                Interaction Design
+              </span>
+            </>
           </h1>
         </div>
       </div>
